c61f5a213350ceeab9e5198bd9a24512b821fdd9
Nevil Vasani <99272374+NevilVasani@users.noreply.github.com>
2022-03-06 10:32:06 +0530

    Enter any 2 numbers and get sum....
    OUTPUT :  Enter Two Numbers:45
                     12
                     45+12=57

---
+++ sum of 2 numbers.cpp
@@ -0,0 +1,17 @@
+//sum of two numbers
+
+#include<iostream>
+using namespace std;
+
+int main()
+{
+	int i,n;
+	cout<<"Enter Two Numbers:";
+	cin>>i>>n;
+	
+	cout<<i<<"+"<<n<<"="<<i+n;
+	
+	
+	return 0;
+}
+
